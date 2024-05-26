43a8c6c366bec730e7559a8297df0dfb6b1a4b82
Charlie Nguyen <vawntanng@gmail.com>
2024-05-26 09:59:47 +0700

    fetch posts and new API get single post

--- sever/app.js
+++ sever/app.js
@@ -1,20 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require('path')
 
-const feedRoutes = require('./routes/feed');
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const feedRoutes = require('./routes/feed')
 
-const app = express();
+const app = express()
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
-app.use(bodyParser.json()); // application/json
+app.use(bodyParser.json()) // application/json
+app.use('./images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
 
-app.use('/feed', feedRoutes);
+app.use('/feed', feedRoutes)
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+  console.log(error)
+  const status = error.statusCode || 500
+  const message = error.message
+  const data = error.data
+  res.status(status).json({ message: message, data: data })
+})
+
+mongoose.connect('mongodb://localhost:27017/node-social-network', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB...')
+  app.listen(8080)
+}).catch(err => console.log(err))
